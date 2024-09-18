1414b8ffed96d6083f57b7571786bae1e6b5efb8
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-09-18 14:19:08 -0700

    initial commit

---
+++ interview-prep/src/test/java/spring_revision_interview_prep/interview_prep/InterviewPrepPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_revision_interview_prep.interview_prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrepPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
