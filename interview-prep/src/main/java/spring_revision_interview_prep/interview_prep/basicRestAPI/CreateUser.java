3b5fd0846a29ba321049245de64240a420b2e9b2
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-09-20 19:06:48 -0700

    Basic @GetMapping Impl

---
+++ interview-prep/src/main/java/spring_revision_interview_prep/interview_prep/basicRestAPI/CreateUser.java
@@ -0,0 +1,20 @@
+package spring_revision_interview_prep.interview_prep.basicRestAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api")
+public class CreateUser {
+    @GetMapping(path="/fetch")
+    public String fetchUser(){
+        return "User details fetched";
+    }
+
+    @GetMapping(path = "/save")
+    public String saveUser(){
+        return "Here are the saved details";
+    }
+}
+
