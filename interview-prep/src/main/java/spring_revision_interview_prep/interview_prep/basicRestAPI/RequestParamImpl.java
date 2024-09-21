c0e0e489b61dbc9a35486239bceb1755930f56e1
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-09-20 19:13:53 -0700

    Basic @RequestParam Impl

---
+++ interview-prep/src/main/java/spring_revision_interview_prep/interview_prep/basicRestAPI/RequestParamImpl.java
@@ -0,0 +1,19 @@
+package spring_revision_interview_prep.interview_prep.basicRestAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api")
+public class RequestParamImpl {
+    @GetMapping(path ="/v1")
+    public String print(
+            @RequestParam(name = "firstName") String firstName,
+            @RequestParam(name = "lastName",required = false) String lastName,
+            @RequestParam(name = "age") int age
+    ){
+        return "My name is " + firstName +" "+lastName +" and my age is " + age;
+    }
+}
